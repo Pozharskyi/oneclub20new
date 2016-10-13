fc1a0005b7ff3d50588bd23277a3dc30c58885ee
Oleksandr Serdiuk <alexandr.serdiuk@icloud.com>
2016-10-13 15:07:53 +0300

    Prepared for upload party

--- public/js/admin/import/uploading/create.js
+++ public/js/admin/import/uploading/create.js
@@ -2,15 +2,16 @@
  * Created by Home on 13.10.2016.
  */
 
-$("#formContent").submit(function(e){
-    e.preventDefault();
-
-    var formdata = new FormData(this);
+// HEREBY
+function uploadFile()
+{
+    var form = document.getElementById("formContent");
+    var formData = new FormData(form);
 
     $.ajax({
         url: "/admin/import/uploading/upload",
         type: "POST",
-        data: formdata,
+        data: formData,
         mimeTypes:"multipart/form-data",
         contentType: false,
         cache: false,
@@ -32,21 +33,51 @@ $("#formContent").submit(function(e){
             getPopup2();
         }
     });
-});
+}
 
 function getPrepareProcess()
 {
 
 }
 
+function getConfirmView()
+{
+    var file = $("#upload").val();
+
+    if( file == '' )
+    {
+        var message =
+            '<div id="alert_status">' +
+                '<div class="text-center">' +
+                    '<h2 class="alert_message">Нужно выбрать файл загрузки</h2>' +
+                    '<button onclick="goBack();" class="btn btn-default">Вернутся</button>' +
+                '</div>' +
+            '</div>';
+    } else
+    {
+        var message =
+            '<div id="alert_status">' +
+                '<div class="text-center">' +
+                    '<h2 class="alert_message">Потвердите загрузку списка</h2>' +
+                    '<button id="left_btn" onclick="goBack();" class="btn btn-default">Отмена</button>' +
+                    '<button id="right_btn" onclick="uploadFile();" class="btn btn-primary">Подвердить</button>' +
+                '</div>' +
+            '</div>';
+    }
+
+    $("#popup_content2").html( message );
+    getPopup2();
+}
+
 function getWaitingView()
 {
-    var message = '<div id="alert_status">' +
-                        '<div class="text-center">' +
-                            '<h2 class="alert_message">Ожидайте, скоро все будет готово...</h2>' +
-                            '<img src="/images/import/loading.gif" />' +
-                        '</div>' +
-                  '</div>';
+    var message =
+        '<div id="alert_status">' +
+            '<div class="text-center">' +
+                '<h2 class="alert_message">Ожидайте, скоро все будет готово...</h2>' +
+                '<img src="/images/import/loading.gif" />' +
+            '</div>' +
+        '</div>';
 
     $("#popup_content2").html( message );
 }
--- resources/views/admin/import/uploading/create.blade.php
+++ resources/views/admin/import/uploading/create.blade.php
@@ -1,3 +1,9 @@
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" type="text/css" href="{{ url('/css/jasny/jasny-bootstrap.min.css') }}">
+
+<!-- Latest compiled and minified JavaScript -->
+<script type="text/javascript" src="{{ url('/js/jasny/jasny-bootstrap.min.js') }}"></script>
+
 <div class="row">
 
     <div class="text-center">
@@ -10,9 +16,28 @@
     <div class="col-md-3"></div>
     <div class="col-md-6">
         <form action="#" id="formContent" method="post" enctype="multipart/form-data" >
-            <input type="file" name="file"  id="upload" required>
+
+            <label for="upload" id="upload_label">Выберите список для загрузки</label>
+            <div class="fileinput fileinput-new input-group" data-provides="fileinput">
+                <div class="form-control" data-trigger="fileinput">
+                    <i class="glyphicon glyphicon-file fileinput-exists"></i>
+                    <span class="fileinput-filename"></span>
+                </div>
+                <span class="input-group-addon btn btn-default btn-file">
+                    <span class="fileinput-new">Выберите файл</span>
+                    <span class="fileinput-exists">Изменить</span>
+                    <input type="file" name="file" id="upload">
+                </span>
+                <a href="#" class="input-group-addon btn btn-default fileinput-exists" data-dismiss="fileinput">Удалить</a>
+            </div>
+
             <input type="hidden" name="party_id" id="party_id" value="{{ $party->id }}" />
-            <button class="submitI" >Upload Image</button>
+
+            <div class="text-center" id="list">
+                <button type="button" id="left_btn" onclick="closePopup();" class="btn btn-default">Отмена</button>
+                <button type="button" id="right_btn" onclick="getConfirmView();" class="btn btn-primary">Загрузить список</button>
+            </div>
+
         </form>
     </div>
     <div class="col-md-3"></div>
