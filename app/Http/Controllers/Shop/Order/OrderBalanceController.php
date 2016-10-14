e852ac1aa80e1772603005e6a919024377b92d2e
Ivan <pozharskyi@gmail.com>
2016-10-14 13:41:28 +0300

    Fix orderBalanceController bug after merge

--- app/Http/Controllers/Shop/Order/OrderBalanceController.php
+++ app/Http/Controllers/Shop/Order/OrderBalanceController.php
@@ -2,10 +2,8 @@
 
 namespace App\Http\Controllers\Shop\Order;
 
-<<<<<<< HEAD
-=======
+
 use App\Http\Controllers\Shop\Basic\BalancesController;
->>>>>>> eea804b80a32838c60736ace1e5b89ae2e7f9d05
 use App\Models\Order\OrderBalanceModel;
 use App\Models\User\UserBalanceModel;
 use Illuminate\Http\Request;
@@ -15,11 +13,9 @@ use App\Http\Controllers\Controller;
 
 class OrderBalanceController extends Controller
 {
-<<<<<<< HEAD
-=======
+
     public static $oldBalanceAmount;
     public static $newBalanceAmount;
->>>>>>> eea804b80a32838c60736ace1e5b89ae2e7f9d05
     /**
      * Update user bonuses if order success
      * @with Transaction
@@ -30,30 +26,25 @@ class OrderBalanceController extends Controller
     {
         $balance = UserBalanceModel::findOrFail( $id );
 
-<<<<<<< HEAD
         $balance->balance_amount -= $balance_amount;
-=======
-        $balance->balance_amount = $balance_amount;
->>>>>>> eea804b80a32838c60736ace1e5b89ae2e7f9d05
 
         $balance->save();
     }
 
+//    /**
+//     * Inserting used bonuses in order
+//     * @param $order_id
+//     * @param $balance_amount
+//     * @return OrderBalanceModel
+//     */
+//    public static function actionInsertBalance( $order_id, $balance_amount )
+//    {
+//        return OrderBalanceModel::create([
+//            'balance_count' => $balance_amount,
+//            'dev_order_index_id' => $order_id,
+//        ]);
+//    }
     /**
-     * Inserting used bonuses in order
-     * @param $order_id
-<<<<<<< HEAD
-     * @param $balance_amount
-     * @return OrderBalanceModel
-     */
-    public static function actionInsertBalance( $order_id, $balance_amount )
-    {
-        return OrderBalanceModel::create([
-            'balance_count' => $balance_amount,
-            'dev_order_index_id' => $order_id,
-        ]);
-    }
-=======
      * @return OrderBalanceModel
      */
     public static function actionInsertBalance( $order_id )
@@ -96,5 +87,4 @@ class OrderBalanceController extends Controller
             return $total;
         }
     }
->>>>>>> eea804b80a32838c60736ace1e5b89ae2e7f9d05
 }
