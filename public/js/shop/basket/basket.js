18861e04e4c0b65a07a0dd855d65bf3f83d3016f
Ivan <pozharskyi@gmail.com>
2016-10-16 17:25:30 +0300

    Fix js get XSRF token if start with space in cookies

--- public/js/shop/basket/basket.js
+++ public/js/shop/basket/basket.js
@@ -8,6 +8,7 @@ var getXsrfToken = function() {
 
     for (var i = 0; i < cookies.length; i++) {
         var cookie = cookies[i].split('=');
+        while (cookie[0].charAt(0)==' ') cookie[0] = cookie[0].substring(1,cookie[0].length);    //if start with space - remove spaces
         if(cookie[0] == 'XSRF-TOKEN') {
             token = decodeURIComponent(cookie[1]);
         }
--- public/js/shop/catalog/description.js
+++ public/js/shop/catalog/description.js
@@ -9,6 +9,7 @@
 
         for (var i = 0; i < cookies.length; i++) {
             var cookie = cookies[i].split('=');
+            while (cookie[0].charAt(0)==' ') cookie[0] = cookie[0].substring(1,cookie[0].length);    //if start with space - remove spaces
             if(cookie[0] == 'XSRF-TOKEN') {
                 token = decodeURIComponent(cookie[1]);
             }
